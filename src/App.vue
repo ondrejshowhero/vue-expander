ff1ce519c57a43a443719595ead4858d2bc7f9a7
Ondrej Muzik <ondrej@kizum.local>
2023-03-07 14:44:20 +0100

    Finished expander

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,87 @@
 
   <section class="section-video">
     <ExpanderVideo :videoId="resourceVideoId" :mobile="mobile" />
+    <div class="logo">
+      <img src="@/assets/ford.svg">
+    </div>
   </section>
 
   <section class="section-main">
-    <h1 class="headin">Some heading here</h1>
+    <h1 class="heading">Nå er Mustang Mach-E et enda smartere bilkjøp: Bedre priser, fast rente og nye kostnadsfrie tjenester</h1>
     <p class="lead">
-      Some introductory text is placed here
+      Leveringstiden på Mustang Mach-E er nå mye kortere enn tidligere. I tillegg har SUV-en blitt et enda smartere bilkjøp, med store produktforbedringer og nye kostnadsfrie tjenester inkludert. Prisene på den helelektriske SUV-en har også blitt enda mer attraktive, med en startpris fra bare 472 300 kroner. 
     </p>
+    <a href="https://ad.doubleclick.net/ddm/trackclk/N9166.4661577SHOWHEROESNO/B29149617.360566977;dc_trk_aid=551362778;dc_trk_cid=184187065;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=" target="_blank" class="cta" @cilck="track('bestill-provekjoring')">Bestill prøvekjøring <span class="desktop-only">av Mustang Mach-E</span> her</a>
+  </section>
+
+  <section class="section-1">
+    <div class="text">
+      <h1 class="heading">Flere kostnadsfrie tjenester</h1>
+      <p class="lead">
+        Med 5 års fri service inkludert sikrer Mustang Mach-E kunder seg et forutsigbart eierforhold. I tillegg vil 3 års gratis dekkhotell med dekkskift og kostnadsfri hente- og bringetjeneste* ved service og EU-kontroll gjøre bilholdet enda enklere og bekymringsfritt.
+      </p>
+    </div>
+    <div class="image">
+      <img src="@/assets/image-1.jpg">
+    </div>
+  </section>
+
+  <section class="section-2">
+    <div class="text">
+      <p class="lead">
+        Vi har det siste årets introdusert forbedringer til Mustang Mach-E som har vært spisset mot den norske kunden. Dette inkluderer blant annet mulighet for taklast, opp til 1 500 kg hengervekt, samt enda mer utstyr som standard. Dette kombinert med mulighet for firehjulstrekk, opp til hele 600 km rekkevidde og et hav av innovative teknologier gjør Mustang Mach-E til det beste bilkjøpet for kunden som ønsker den smarteste- og tøffeste elbilen på markedet, sier administrerende direktør i Ford Motor Norge, Per Gunnar Berg. 
+      </p>
+    </div>
+    <div class="image">
+      <img src="@/assets/image-2.jpg">
+    </div>
+  </section>
+
+  <section class="section-3">
+      <h1 class="heading">Gunstigere finansiering og 5&nbsp;års fri&nbsp;service</h1>
+      <p class="lead">
+        Ved kjøp av ny Mustang Mach-E får kunder nå tilbud om 3,49 % fastrente i 3 år** som gjelder både ved lån og privatleasing
+      </p>
+      <p class="lead">
+        – En av våre store styrker er et omfattende nettverk med over 70 Ford-forhandlere i hele Norge som står klare til å gi kundene personlig service – hver eneste dag. Vi skal gjøre alt for at et Mustang Mach-E eierskap ikke bare skal være morsomt men så rimelig, enkelt og tidsbesparende som mulig, sier Ford Motor Norges administrerende direktør.
+      </p>
   </section>
 
   <section class="section-scroller">
     <ExpanderScroller @track="track" />
   </section>
+
+  <section class="section-4">
+    <h1 class="heading">Fortsetter produkt- og tjenesteforbedringene fra&nbsp;2022</h1>
+    <p class="lead">
+      I løpet av 2022 kom også Ford med en rekke produktforbedringer på Mustang Mach-E – for å gjøre bilen enda mer skreddersydd for norske kunder, blant annet økt tilhengervekt på opptil 1500 kg og mulighet for taklast.<br>
+      – Med disse endringene har en av de beste og råeste elbilene blitt aktuell for enda flere kunder. Dette er også en av bilene på markedet med lengst rekkevidde og mest omfattende muligheter for over-the-air (OTA) oppdateringer, noe de eksisterende kundene allerede har merket, og i enda større grad vil oppleve i de kommende månedene. Vi liker å si at det er din bil, men det er fortsatt vår baby, avslutter administrerende direktør i Ford Motor Norge, Per Gunnar Berg.
+    </p>
+  </section>
+
+  <footer class="section-footer">
+    <div class="logo">
+      <img src="@/assets/logo.svg">
+    </div>
+    <a href="https://ad.doubleclick.net/ddm/trackclk/N9166.4661577SHOWHEROESNO/B29149617.360566974;dc_trk_aid=551516881;dc_trk_cid=184187065;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=" target="_blank" class="cta" @cilck="track('les-mer')">Les mer</a>
+  </footer>
      
-  <section class="section-slider">
-    <ExpanderSlider @track="track" />
-  </section>
-
-  <section class="section-countdown">
-    <ExpanderCountdown />
-  </section>
-
-
 </template>
 
 <script>
   import ExpanderVideo from './components/ExpanderVideo.vue'
   import ExpanderScroller from './components/ExpanderScroller.vue'
-  import ExpanderSlider from './components/ExpanderSlider.vue'
-  import ExpanderCountdown from './components/ExpanderCountdown.vue'
 
   export default {
     name: 'App',
     components: {
       ExpanderVideo,
       ExpanderScroller,
-      ExpanderSlider,
-      ExpanderCountdown,
     },
     data() {
       return {
-        resourcesId: 1488,
-        resourceVideoId: 2,
+        resourcesId: 2053,
+        resourceVideoId: 2683,
         version: 1,
         mobile: false
       }
@@ -52,8 +91,10 @@
       let uri = window.location.search.substring(1)
       let params = new URLSearchParams(uri)
       let id = params.get('version')
-      if (id == 2) {
-        this.version = 2
+      if (id == 'mobile') {
+        // this.version = 2
+        this.mobile = true
+        this.resourceVideoId = 2684
       }
     },
     methods: {
@@ -66,7 +107,7 @@
 
 <style lang="scss">
   @import "node_modules/minireset.css/minireset";
-  // @import '@/scss/fonts.scss';
+  @import '@/scss/fonts.scss';
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
@@ -92,6 +133,10 @@
     background: $color-bg;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    padding-bottom: 3.25rem;
+    @include d {
+      padding-bottom: 3.875rem;
+    }
   }
 
   img {
@@ -119,5 +164,216 @@
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
+  .section-video {
+    position: relative;
+    .logo {
+      position: absolute;
+      left: 50%;
+      width: 35vw;
+      bottom: 4vw;
+      transform: translate(-50%, 0);
+      z-index: 1000;
+      @include d {
+        bottom: 2rem;
+        width: 18.75rem;
+      }
+    }
+  }
+
+  .heading {
+    font-size: 1.5rem;
+    font-weight: bold;
+    letter-spacing: .055em;
+    @include d {
+      font-size: 1.5rem;
+    }
+  }
+  .lead {
+    font-size: 1rem;
+    letter-spacing: .055em;
+    line-height: 1.222;
+    @include d {
+      font-size: 1.125rem;
+    }
+  }
+  .cta {
+    display: inline-block;
+    padding: .875rem 1.25rem;
+    font-size: 1.25rem;
+    border: 1px solid #fff;
+    border-radius: 3rem;
+    transition: .15s;
+    letter-spacing: .055em;
+    .desktop-only {
+      display: none;
+      @include d {
+        display: inline-b;
+      }
+    }
+    @include d {
+      padding: .875rem 1.875rem;
+      font-size: 1.5rem;
+    }
+    &:hover {
+      background: #fff;
+      color: #000;
+    }
+  }
+
+  
+  .section-main {
+    padding: 3rem 1rem;
+    text-align: center;
+    background: linear-gradient(95.61deg, rgba(0, 0, 0, 0.61) 27.7%, rgba(0, 0, 0, 0) 161.84%), url('assets/bg-1.jpg') no-repeat center center / cover;
+    @include d {
+      padding: 5rem 1rem 4.25rem;
+    }
+    .heading {
+      max-width: 52.25rem;
+      margin: 0 auto 1rem;
+      font-size: 1.5rem;
+      line-height: 1.1875;
+      @include d {
+        font-size: 2rem;
+      }
+    }
+    .lead {
+      margin: 0 auto 2.5rem;
+      max-width: 48rem;
+      font-weight: 500;
+    }
+  }
+
+  .section-1 {
+    @include d {
+      display: flex;
+      .text,
+      .image {
+        width: 50%;
+      }
+      .text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+      }
+    }
+    .text {
+      padding: 2rem;
+      @include d {
+        padding: 3.125rem;
+      }
+    }
+    .heading {
+      margin-bottom: 1rem;
+    }
+  }
+
+  .section-2 {
+    position: relative;
+    .text {
+      position: relative;
+      padding: 4rem 2rem;
+      @include d {
+        padding: 0;
+        position: absolute;
+        right: 1.875rem;
+        bottom: -6.75rem;
+        width: 24.75rem;
+        padding: 1.625rem;
+        background: #000;
+        z-index: 100;
+      }
+      &:before,
+      &:after {
+        content: '';
+        position: absolute;
+        width: 3rem;
+        height: 3rem;
+        @include d {
+          width: 4.25rem;
+          height: 4.25rem;
+        }
+      }
+      &::before {
+        left: 1rem;
+        top: 1rem;
+        background: url('assets/quote-start.png') no-repeat center center / contain;
+        @include d {
+          left: -.25rem;
+          top: -2.375rem;
+        }
+      }
+      &::after {
+        right: 1rem;
+        bottom: 1rem;
+        background: url('assets/quote-end.png') no-repeat center center / contain;
+        @include d {
+          right: 1.625rem;
+          bottom: -1rem;
+        }
+      }
+    }
+  }
+
+  .section-3 {
+    padding: 2.5rem 2rem;
+    @include d {
+      padding: 6.75rem 3.25rem 2.5rem;
+    }
+    .heading {
+      margin-bottom: 1rem;
+    }
+    .lead {
+      max-width: 49.875rem;
+      margin-bottom: 1.125rem;
+    }
+  }
+
+  .section-4 {
+    margin-top: 3rem;
+    padding: 3rem 2rem;
+    background: linear-gradient(101.81deg, rgba(0, 0, 0, 0.46) 26.91%, rgba(0, 0, 0, 0) 73.57%), url('assets/bg-2.jpg') no-repeat center 72% / cover;
+    @include d {
+      margin-top: 5rem;
+      padding: 6.25rem 5.25rem 5.25rem;
+    }
+    .heading {
+      margin-bottom: 1rem;
+    }
+  }
+
+  .section-footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 10000;
+    width: 100%;
+    padding: .375rem 1.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #000;
+    @include d {
+      padding: .375rem 3.5rem;
+    }
+    .logo {
+      width: 5rem;
+      @include d {
+        width: 6.375rem;
+      }
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+    .cta {
+      font-size: 1.125rem;
+      @include m {
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+      }
+    }
+  }
 
 </style>
--- src/components/ExpanderScroller.vue
+++ src/components/ExpanderScroller.vue
@@ -2,15 +2,22 @@
   <div class="scroll-container" :class="{leftmost: leftMost, rightmost: rightMost}" ref="container">
     <div class="scroller" ref="scroller" @scroll="scrollerScrolled">
       <div class="items" ref="items">
-        <a :href="item[3]" target="_blank" class="item" v-for="item in scrollerItems" :key="item" @click="$emit('track', item[3])">
+        <a href="https://ad.doubleclick.net/ddm/trackclk/N9166.4661577SHOWHEROESNO/B29149617.360566974;dc_trk_aid=551516881;dc_trk_cid=184187065;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=" target="_blank" class="item" v-for="item in scrollerItems" :key="item" @click="$emit('track', `product-${item[4]}`)">
           <div class="image">
-            <img :src="getImageUrl(item[2])">
+            <img :src="getImageUrl(item[4])">
           </div>
           <div class="info">
-            <h3 class="title">{{item[0]}}</h3>
-            <p>
-              {{item[1]}}
+            <p class="distance">{{item[3]}} km rekkevidde</p>
+            <h2 class="title">{{item[0]}}</h2>
+            <p class="price">
+              <span>Ny veil. pris ink. levering:</span> Kr. {{item[1]}}
             </p>
+            <template v-if="item[5]">
+              <h2 class="title">{{item[5]}}</h2>
+              <p class="price">
+                <span>Ny veil. pris ink. levering:</span> Kr. {{item[6]}}
+              </p>
+            </template>
           </div>
         </a>
       </div>
@@ -28,11 +35,39 @@
       return {
         scrollerItems: [
           [
-            'Title',
-            'The text',
-            'image.jpg',
-            'https://showheroes.com',
-            'title'
+            'Standard Range RWD:',
+            '472 300',
+            'car-1.jpg',
+            '440',
+            'standard-range-rwd'
+          ],
+          [
+            'Premium Long Range RWD:',
+            '554 300',
+            'car-2.jpg',
+            '600',
+            'premium-long-range-rwd'
+          ],
+          [
+            'Premium Standard Range AWD:',
+            '553 800',
+            'car-3.jpg',
+            '400',
+            'premium-standard-range-awd',
+          ],
+          [
+            'Premium Long Range AWD:',
+            '597 400',
+            'car-1.jpg',
+            '550',
+            'premium-long-range-awd'
+          ],
+          [
+            'Mach-E GT:',
+            '681 700',
+            'car-1.jpg',
+            '490',
+            'mach-e-gt'
           ],
         ],
         leftMost: true,
@@ -41,7 +76,7 @@
     },
     methods: {
       getImageUrl(image) {
-        return require(`@/assets/${image}`)
+        return require(`@/assets/cars/${image}.png`)
       },
       scrollToPrev: function() {
         let w = this.$refs.items.children[0].offsetWidth
@@ -84,49 +119,113 @@
   }
   .scroller {
     overflow: auto;
-    scroll-snap-type: x mandatory;
     padding-bottom: 1rem;
+    @include m {
+      scroll-snap-type: x mandatory;
+    }
   }
   .items {
     display: inline-flex;
     justify-content: space-between;
-    align-items: flex-start;
-    padding: 0 var(--gap);
+    align-items: stretch;
+    padding: 0 15vw;
+    @include d {
+      padding: 0 2.25rem;
+    }
   }
   .item {
     display: block;
-    width: 60vw;
-    scroll-snap-align: center;
+    width: 70vw;
+    padding: 1.375rem .875rem 0;
+    border-radius: .5rem;
+    background: #fff;
+    overflow: hidden;
+    color: #000;
+    text-align: center;
+    @include m {
+      scroll-snap-align: center;
+    }
     @include d {
-      width: 27.75rem;
+      width: 20.375rem;
+    }
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 11.875rem;
+    }
+    .distance {
+      position: relative;
+      top: -2rem;
+      font-weight: bold;
+    }
+    .title {
+      margin-bottom: .25rem;
+      font-size: 1.125rem;
+      font-weight: bold;
+    }
+    .price {
+      font-size: 1.125rem;
+      span {
+        font-size: 1rem;
+      }
+      strong {
+        display: block;
+        margin-top: .25rem;
+        font-size: 1.25rem;
+      }
+    }
+    .image {
+      overflow: hidden;
+      img {
+        transition: .3s;
+        transform: translate3d(0,0,0);
+      }
+    }
+    &:hover {
+      .image {
+        img {
+          transform: translate3d(0,0,0) scale(1.03);
+        }
+      }
     }
   }
   .item + .item {
     margin-left: 1rem;
     @include d {
-      margin-left: 2.5rem;
+      margin-left: 1.625rem;
     }
   }
 
   .nav {
     position: absolute;
-    top: 40vw;
+    top: 0;
     z-index: 10;
-    width: 4rem;
-    height: 4rem;
-    margin-top: -2rem;
+    width: 15vw;
+    height: 100%;
     border: 0;
     overflow: hidden;
     cursor: pointer;
     text-indent: -999em;
-    display: none;
+    @include d {
+      width: 8rem;
+    }
     &.prev {
       left: 0;
-      // background: url('../assets/arr-l.svg') no-repeat center center / 50% auto;
+      background: url('../assets/arr-l.svg') no-repeat 25% center / 8vw auto, linear-gradient(90deg, #000000 6.39%, rgba(0, 0, 0, 0) 91.55%);
+      @include d {
+        background-size: 4rem auto, auto;
+        background-position: center center, center;
+      }
     }
     &.next {
       right: 0;
-      // background: url('../assets/arr-r.svg') no-repeat center center / 50% auto;
+      background: url('../assets/arr-r.svg') no-repeat 75% center / 8vw auto, linear-gradient(270deg, #000000 6.39%, rgba(0, 0, 0, 0) 91.55%);
+      @include d {
+        background-size: 4rem auto, auto;
+        background-position: center center, center;
+      }
     }
   }
 
