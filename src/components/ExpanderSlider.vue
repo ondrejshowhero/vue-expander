8401eb86afac2c4666a39dcad16b27b3cbe23d1b
Ondrej Muzik <ondrej@kizum-87.docsis.vodafone.cz>
2024-06-11 09:53:12 +0200

    Finished campaign

--- src/components/ExpanderSlider.vue
+++ src/components/ExpanderSlider.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="wrapper">
     <div class="slider" ref="slider">
-      <div class="item" v-for="item in 5" :key="item">
-        <a href="" target="_blank" class="inner" @click="$emit('track', 'test')">
-          <img :src="getImage(item)">
+      <article class="item" v-for="item in items" :key="item">
+        <a href="https://secure.adnxs.com/clktrb?id=802715&redir=https://ad.doubleclick.net/ddm/trackclk/N9166.4661577SHOWHEROESNO/B31787091.391082153;dc_trk_aid=582555944;dc_trk_cid=212504900;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank" class="inner" @click="$emit('track', 'test')">
+          <div class="text">
+            <h1 class="title">{{ item[0] }}</h1>
+            <div v-html="item[1]"></div>
+            <a href="https://secure.adnxs.com/clktrb?id=802715&redir=https://ad.doubleclick.net/ddm/trackclk/N9166.4661577SHOWHEROESNO/B31787091.391082153;dc_trk_aid=582555944;dc_trk_cid=212504900;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank" class="cta">Les mer</a>
+          </div>
+          <div class="image">
+            <img :src="getImage(item[2])">
+          </div>
         </a>
-      </div>
+      </article>
     </div>
   </div>
 </template>
@@ -16,23 +23,51 @@
   export default {
     name: 'ExpanderSlider',
     emits: ['track'],
+    data() {
+      return {
+        items: [
+          [
+            'Ekstrem fremkommelighet og svært gode kjøreegenskaper',
+            `<p>Med firehjulstrekk som standard, et uavhengig bakhjulsoppheng, og et lavt tyngdepunkt, er kjøreegenskapene og fremkommeligheten til F-150 Lightning svært gode på alle underlag. Den kommer også med fire forskjellige kjøremoduser som automatisk optimaliserer den for forskjellige kjørescenarioer: Normal, Sport, Off-road og Tauing.</p>
+            <p>Understellet er forsterket for å beskytte batteri og motor i tøft terreng. Batterikassen er vanntett og beskyttet av et støtabsorberende materiale.</p>`,
+            'image-1.jpg'
+          ],
+          [
+            'Enormt med plass til 5 personer og bagasje',
+            '<p>Nye F-150 Lightning er en svært stor og romslig pickup for fem personer. Den er hele 5,91 meter lang, 2,44 meter bred og 1,99 meter høy. I tillegg til et stort plan og mye plass inni kupeen, har den en drenerbar Mega-Frunk på hele 400 liter under panseret hvor du kan frakte mer enn 180 kilo.</p>',
+            'image-2.jpg',
+          ],
+          [
+            'Det siste og beste av teknologi og smarte løsninger',
+            '<p>Nye F-150 Lightning kommer blant annet som standard med smarte løsninger som Pro Trailer Backup-assistent som gjør at du enkelt styrer rygging med henger fra kupeen med et roteringshjul og ryggekamera. Med Pro Power om bord kan bilen fungere som strømkilde med inntil 2,3 kW. Du får varme og kjøling i forsetene og et premium lydanlegg fra B&O.</p>',
+            'image-3.jpg'
+          ]
+        ]
+      }
+    },
     mounted() {
       tns({
         container: this.$refs.slider,
         items: 1,
-        gutter: 20,
+        gutter: 0,
         controls: false,
-        nav: false,
+        nav: true,
         autoplay: true,
-        autoplayTimeout: 2000,
+        autoplayTimeout: 8000,
         autoplayButtonOutput: false,
         speed: 750,
-        loop: true
+        autoHeight: true,
+        loop: true,
+        responsive: {
+          480: {
+            autoHeight: false
+          }
+        }
       });
     },
     methods: {
       getImage(image) {
-        return require(`@/assets/${image}`)
+        return require(`@/assets/slider/${image}`)
       }
     }
   }
@@ -49,11 +84,47 @@
   .wrapper {
     position: relative;
     .inner {
-      display: block;
-      img {
-        display: block;
-        width: 100%;
+      margin: 0 2.375rem;
+      box-shadow: 0rem 1.875rem 2.75rem 0rem rgba(0, 0, 0, 0.15), 0rem .25rem 1.5rem 0rem rgba(0, 0, 0, 0.19);
+      border-radius: 0.875rem;
+      background: #fff;
+      overflow: hidden;
+      @include d {
+        display: flex;
+        justify-content: space-between;
+        .text {
+          width: 33.125rem;
+        }
+        .image {
+          width: 29.625rem;
+        }
       }
+      .text {
+        padding: 4.375rem 2rem 2rem;
+        color: #333;
+        @include d {
+          padding: 4.375rem 3.375rem 1rem 4rem;
+        }
+        .title {
+          margin-bottom: 1.5rem;
+          font-size: 2rem;
+          font-weight: 700;
+        }
+        :deep(p) {
+          max-width: 22.125rem;
+        }
+        :deep(p + p) {
+          margin-top: 1rem;
+        }
+        .cta {
+          margin-top: 1.125rem;
+          padding: .9375rem 2.75rem;
+        }
+      }
+    }
+
+    :deep(.tns-ovh) {
+      padding: 4rem 0 11.25rem;
     }
     
     :deep(.tns-controls) {
@@ -92,21 +163,24 @@
     :deep(.tns-nav) {
       outline: 0;
       position: absolute;
-      left: 0;
-      width: 100%;
-      bottom: 3rem;
+      left: 2rem;
+      top: 6.5rem;
+      z-index: 100;
       display: flex;
       justify-content: center;
       align-items: center;
+      @include d {
+        left: 6.5rem;
+      }
       button {
-        width: .5rem;
-        height: .5rem;
-        margin: 0 .375rem;
-        background: #888;
-        border-radius: 50%;
+        width: 2.625rem;
+        height: 0.25rem;
+        margin: 0 .4375rem 0 0;
+        background: #989898;
+        border-radius: 0.125rem;
       }
       .tns-nav-active {
-        background: #fff;
+        background: $color-text;
       }
     }
   }
