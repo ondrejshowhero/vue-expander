062e332107f9dd8dd45b17619831fe84eec0b520
Ondrej Muzik <ondrej@kizum-557.docsis.vodafone.cz>
2024-08-15 10:13:40 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,123 @@
 <template>
 
   <section class="section-video">
-    <ExpanderVideo :filename="videoFile" :mobile="mobile" :autoplay="true" url="" />
+    <ExpanderVideo :filename="videoFile" :autoplay="true" :url="theUrl" />
   </section>
 
   <section class="section-main">
-    <h1 class="heading">Some heading here</h1>
-    <p class="lead">
-      Some introductory text is placed here
-    </p>
-    <a href="" target="_blank" class="cta">Read more</a>
-  </section>
-
-  <section class="section-scroller">
-    <ExpanderScroller />
-  </section>
-     
-  <section class="section-slider">
-    <ExpanderSlider />
-  </section>
-
-  <footer class="section-footer sticky">
-    <div class="container">
-      <a href="" target="_blank" class="logo">
-        <img src="@/assets/logo.png">
-      </a> 
-      <a href="" target="_blank" class="cta">Read more</a>
+    <div class="text">
+      <h1 class="heading">
+        En <em>gave</em> som ingen anden<br>
+        Til enhver anledning
+      </h1>
+      <p class="lead">
+        En håndmalet gave fra Royal Copenhagen er noget helt særligt. Elegant indpakket i blå og straks genkendelig. Den bærer århundreders dansk designhistorie indeni og kommer til at skabe glæde hver eneste dag.
+      </p>
     </div>
-  </footer>
+    <div class="image">
+      <img src="@/assets/image-header.jpg">
+    </div>
+  </section>
 
+  <section class="section-tiles">
+
+    <div class="tile">
+      <div class="text">
+        <h2 class="heading">
+          Glæd dig til 
+          <em>uforglemmelige</em> stunder
+        </h2>
+        <p>
+          Tilføj dine favoritter til din ønskeliste og lad sirlige håndmalede penselstrøg tilføre en unik skønhed til dit hjem.
+        </p>
+        <a :href="theUrl" target="_blank" class="cta">FÅ INSPIRATION</a>
+      </div>
+      <div class="image">
+        <img src="@/assets/image-1.jpg">
+      </div>
+    </div>
+
+    <div class="tile">
+      <div class="text">
+        <h2 class="heading">
+          Giv en gave, der skaber <em>minder</em>
+        </h2>
+        <p>
+          Uanset om det er en fødselsdag, en årsdag eller enhver anden særlig begivenhed, vil et stykker Royal Copenhagen-porcelæn være en gave, der skaber glæde og minder for livet.
+        </p>
+      </div>
+      <div class="image">
+        <img src="@/assets/image-2.jpg">
+      </div>
+    </div>
+
+    <div class="tile">
+      <div class="text">
+        <h2 class="heading">
+          Til <em>enhver</em> anledning
+        </h2>
+        <p>
+          Livets små og store øjeblikke fortjener at blive fejret. Inviter på en uforglemmelig dag omgivet af et festligt bord dækket med Royal Copenhagens blåmalede stel. Smukt til enhver anledning.
+        </p>
+        <a :href="theUrl" target="_blank" class="cta">FÅ INSPIRATION</a>
+      </div>
+      <div class="image">
+        <img src="@/assets/image-3.jpg">
+      </div>
+    </div>
+
+    <div class="tile">
+      <div class="text">
+        <h2 class="heading">Den ekstra detalje</h2>
+        <p>
+          Royal Copenhagens gaveindpakning er næsten lige så særlig som porcelænet selv. Inspireret af origamikunst, udfolder indpakningspapiret på en Royal Copenhagen-gave sig på smukkeste vis som en blomst, når båndet løsnes.
+        </p>
+      </div>
+      <div class="image">
+        <img src="@/assets/image-4.jpg">
+      </div>
+    </div>
+
+  </section>
+
+  <div class="bg">
+     
+    <section class="section-slider">
+      <ExpanderScroller :mobile="mobile" />
+    </section>
+
+    <footer class="section-footer">
+      <div class="container">
+        <a :href="theUrl" target="_blank" class="logo">
+          <img src="@/assets/logo.png">
+        </a> 
+      </div>
+    </footer>
+
+  </div>
 
 </template>
 
 <script>
   import ExpanderVideo from './components/ExpanderVideo.vue'
   import ExpanderScroller from './components/ExpanderScroller.vue'
-  import ExpanderSlider from './components/ExpanderSlider.vue'
 
   export default {
     name: 'App',
     components: {
       ExpanderVideo,
       ExpanderScroller,
-      ExpanderSlider,
     },
     data() {
       return {
-        mobile: false,
-        videoFile: '1080.mp4'
+        mobile: true,
+        videoFile: '1080.mp4',
+        theUrl: 'https://secure.adnxs.com/clktrb?id=803853&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584015636;dc_trk_cid=213355345;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1'
       }
     },
     created() {
-      // if (window.innerWidth < 480) {
-      //   this.mobile = true
-      //   this.videofile = '480.mp4'
-      // }
+      if (this.mobile)
+        this.theUrl = 'https://secure.adnxs.com/clktrb?id=803857&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584016725;dc_trk_cid=213493649;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1'
     },
   }
 </script>
@@ -64,13 +127,6 @@
   @import '@/scss/fonts.scss';
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
-
-  :root {
-    --footer-height: 2rem;
-    @include d {
-      --footer-height: 4rem;
-    }
-  }
 
   html {
     font-family: $font, sans-serif;
@@ -94,7 +150,6 @@
     background: $color-bg;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    padding-bottom: var(--footer-height);
   }
 
   img,
@@ -119,30 +174,134 @@
     cursor: pointer;
   }
 
+  .cta {
+    display: inline-block;
+    padding: 1.125rem 4.375rem;
+    background: $color-1;
+    font-weight: bold;
+    color: #fff;
+    &:hover {
+      background: #19395b;
+    }
+  }
+
 </style>
 
 <style scoped lang="scss">
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
+  .heading {
+    font-family: $font-headings;
+    font-weight: bold;
+    font-size: 1.25rem;
+    @include d {
+      font-size: 1.875rem;
+    }
+  }
+
+  .section-main {
+    padding: 2.75rem 0 0;
+    text-align: center;
+    .text {
+      position: relative;
+      z-index: 2;
+      .heading {
+        width: 24rem;
+        max-width: 90%;
+        margin: 0 auto 1.125rem;
+        font-size: 1.75rem;
+        @include d {
+          font-size: 2.125rem;
+        }
+      }
+      .lead {
+        width: 35.875rem;
+        max-width: 90%;
+        margin: 0 auto;
+        font-size: 1.125rem;
+        @include d {
+          font-size: 1.3125rem;
+        }
+      }
+    }
+    .image {
+      position: relative;
+      margin-top: -2rem;
+      @include d {
+        margin-top: -5.75rem;
+      }
+      &::after {
+        content: '';
+        @include cover;
+        background: linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.61) 28.74%, rgba(255, 255, 255, 0.00) 64.66%);
+      }
+    }
+  }
+
+  .section-tiles {
+    .tile {
+      @include d {
+        display: flex;
+        align-items: center;
+        div {
+          flex: 1 0;
+        }
+      }
+      .text {
+        padding: 2rem 1.5rem;
+        text-align: right;
+        @include d {
+          padding: 0 0 0 6.25rem;
+          text-align: left;
+        }
+        .heading,
+        p {
+          max-width: 21.875rem;
+        }
+        .heading {
+          margin-bottom: 1rem;
+          @include d {
+            margin-bottom: 1.875rem;
+          }
+        }
+        p {
+          font-size: 1.125rem;
+          @include d {
+            font-size: 1.25rem;
+          }
+        }
+        .cta {
+          margin-top: 1.875rem;
+        }
+      }
+      &:nth-child(odd) {
+        flex-direction: row-reverse;
+        .text {
+          text-align: left;
+          @include d {
+            padding: 0 0 0 5.625rem;
+          }
+        }
+      }
+    }
+  }
+
+  .bg {
+    padding: 5.875rem 0 0;
+    background: url('@/assets/image-footer.jpg') no-repeat center center / cover;
+  }
+
   .section-footer {
     .container {
       display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: var(--footer-height);
-      padding: 0 1rem;
-      background: #fff;
+      justify-content: center;
+      padding: 3rem 1rem;
     }
-    &.sticky {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      z-index: 1000;
-      width: 100%;
-      .container {
-        max-width: $width;
-        margin: 0 auto;
+    .logo {
+      width: 12rem;
+      @include d {
+        width: 15.25rem;
       }
     }
   }
--- src/components/ExpanderScroller.vue
+++ src/components/ExpanderScroller.vue
@@ -2,15 +2,18 @@
   <div class="scroll-container" :class="{leftmost: leftMost, rightmost: rightMost}" ref="container">
     <div class="scroller" ref="scroller" @scroll="scrollerScrolled">
       <div class="items" ref="items">
-        <a :href="item[3]" target="_blank" class="item" v-for="item in scrollerItems" :key="item" @click="$emit('track', item[3])">
+        <a :href="getUrl(item[3])" target="_blank" class="item" v-for="item in scrollerItems" :key="item">
+          <div class="info">
+            <p>
+              {{item[0]}}
+            </p>
+            <h3 class="title">{{item[1]}}</h3>
+          </div>
           <div class="image">
             <img :src="getImageUrl(item[2])">
           </div>
-          <div class="info">
-            <h3 class="title">{{item[0]}}</h3>
-            <p>
-              {{item[1]}}
-            </p>
+          <div class="cta">
+            SHOP NU
           </div>
         </a>
       </div>
@@ -23,16 +26,38 @@
 <script>
   export default {
     name: 'ExpanderScroller',
-    emits: ['track'],
+    props: {
+      mobile: Boolean
+    },
     data() {
       return {
         scrollerItems: [
           [
-            'Title',
-            'The text',
-            'image.jpg',
-            'https://showheroes.com',
-            'title'
+            'Blå Mega Riflet',
+            'BILLEDRAMME, 21 CM',
+            'product-1.png',
+            [
+              'https://secure.adnxs.com/clktrb?id=803856&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584460345;dc_trk_cid=213639958;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+              'https://secure.adnxs.com/clktrb?id=803860&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584402825;dc_trk_cid=213756821;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+            ]
+          ],
+          [
+            'Musselmalet Riflet',
+            'TALLERKEN 22 CM',
+            'product-2.png',
+            [
+              'https://secure.adnxs.com/clktrb?id=803854&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584403095;dc_trk_cid=213755489;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+              'https://secure.adnxs.com/clktrb?id=803858&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584402828;dc_trk_cid=213756824;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+            ]
+          ],
+          [
+            'Koral Musselmalet Halvblonde',
+            'HØJHANKSKOP 33 CL',
+            'product-3.png',
+            [
+              'https://secure.adnxs.com/clktrb?id=803855&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584460051;dc_trk_cid=213755492;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+              'https://secure.adnxs.com/clktrb?id=803859&redir=https://ad.doubleclick.net/ddm/trackclk/N2300581.3993854SHOWHEROES/B31885389.392711319;dc_trk_aid=584460348;dc_trk_cid=213756827;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+            ]
           ],
         ],
         leftMost: true,
@@ -42,8 +67,13 @@
       }
     },
     methods: {
+      getUrl(urls) {
+        if (this.mobile)
+          return urls[1]
+        return urls[0]
+      },
       getImageUrl(image) {
-        return require(`@/assets/${image}`)
+        return require(`@/assets/products/${image}`)
       },
       scrollToPrev: function() {
         let w = this.$refs.items.children[0].offsetWidth
@@ -94,33 +124,54 @@
     position: relative;
   }
   .scroller {
-    overflow: auto;
-    scroll-snap-type: x mandatory;
-    padding-bottom: 1rem;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+    @include m {
+      overflow: auto;
+      scroll-snap-type: x mandatory;
+      -ms-overflow-style: none;
+      scrollbar-width: none;
+    }
   }
   .scroller::-webkit-scrollbar {
     display: none;
   }
   .items {
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding: 0 1rem;
+    display: flex;
+    justify-content: center;
+    @include m {
+      display: inline-flex;
+      align-items: flex-start;
+      padding: 0 20vw;
+    }
   }
   .item {
     display: block;
     width: 60vw;
+    padding-bottom: 1.375rem;
     scroll-snap-align: center;
+    background: #fff;
+    text-align: center;
     @include d {
-      width: 27.75rem;
+      width: 16.5rem;
+    }
+    .info {
+      padding: 2.125rem 1rem .5rem;
+      p {
+        font-size: .75rem;
+      }
+      .title {
+        margin-top: .5rem;
+        font-family: $font-headings;
+        font-size: 1.125rem;
+      }
+    }
+    .image {
+      margin: 0 2.25rem .5rem;
     }
   }
   .item + .item {
-    margin-left: 1rem;
+    margin-left: 7.5vw;
     @include d {
-      margin-left: 2.5rem;
+      margin-left: 2.75rem;
     }
   }
 
