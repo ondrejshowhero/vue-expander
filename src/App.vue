5bca0d6d11ba85f613717c7ac8110844448bb314
Ondrej Muzik <ondrej@kizum.docsis.vodafone.cz>
2024-08-15 09:29:20 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,46 @@
 <template>
 
   <section class="section-video">
-    <ExpanderVideo :filename="videoFile" :mobile="mobile" :autoplay="true" url="" />
+    <ExpanderVideo :filename="videoFile" :autoplay="true" url="https://ad.doubleclick.net/ddm/trackclk/N6344.3993854SHOWHEROES/B32418753.400181035;dc_trk_aid=592303272;dc_trk_cid=219589835;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" />
   </section>
 
   <section class="section-main">
-    <h1 class="heading">Some heading here</h1>
-    <p class="lead">
-      Some introductory text is placed here
-    </p>
-    <a href="" target="_blank" class="cta">Read more</a>
+    <picture>
+      <source :srcset="getDesktopImage()" media="(min-width: 480px)" />
+      <img :src="getMobileImage()">
+    </picture>
+    <a href="https://ad.doubleclick.net/ddm/trackclk/N6344.3993854SHOWHEROES/B32418753.400181035;dc_trk_aid=592303272;dc_trk_cid=219589835;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank" class="link"></a>
+    <a href="https://ad.doubleclick.net/ddm/trackclk/N6344.3993854SHOWHEROES/B32418753.400181035;dc_trk_aid=592303272;dc_trk_cid=219589835;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank" class="cta"><img src="@/assets/cta.png" alt="Tilmeld dig"></a>
   </section>
-
-  <section class="section-scroller">
-    <ExpanderScroller />
-  </section>
-     
-  <section class="section-slider">
-    <ExpanderSlider />
-  </section>
-
-  <footer class="section-footer sticky">
-    <div class="container">
-      <a href="" target="_blank" class="logo">
-        <img src="@/assets/logo.png">
-      </a> 
-      <a href="" target="_blank" class="cta">Read more</a>
-    </div>
-  </footer>
-
 
 </template>
 
 <script>
   import ExpanderVideo from './components/ExpanderVideo.vue'
-  import ExpanderScroller from './components/ExpanderScroller.vue'
-  import ExpanderSlider from './components/ExpanderSlider.vue'
 
   export default {
     name: 'App',
     components: {
       ExpanderVideo,
-      ExpanderScroller,
-      ExpanderSlider,
     },
     data() {
       return {
         mobile: false,
-        videoFile: '1080.mp4'
+        // videoFile: '1080.mp4',
+        version: 'post',
+      }
+    },
+    computed: {
+      videoFile() {
+        return `1080-${this.version}.mp4`
+      }
+    },
+    methods: {
+      getDesktopImage() {
+        return require(`@/assets/${this.version}/desktop.png`)
+      },
+      getMobileImage() {
+        return require(`@/assets/${this.version}/mobile.png`)
       }
     },
     created() {
@@ -64,13 +57,6 @@
   @import '@/scss/fonts.scss';
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
-
-  :root {
-    --footer-height: 2rem;
-    @include d {
-      --footer-height: 4rem;
-    }
-  }
 
   html {
     font-family: $font, sans-serif;
@@ -125,25 +111,50 @@
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
-  .section-footer {
-    .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: var(--footer-height);
-      padding: 0 1rem;
-      background: #fff;
+  .section-main {
+    position: relative;
+    background: url('@/assets/bg.jpg') no-repeat center center / auto 150%;
+    animation: bgmobile 5s ease-in-out 0s infinite alternate forwards;
+    @include d {
+      background-size: 150% auto;
+      animation: bg 5s ease-in-out 0s infinite alternate forwards;
     }
-    &.sticky {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      z-index: 1000;
-      width: 100%;
-      .container {
-        max-width: $width;
-        margin: 0 auto;
+    .link {
+      @include cover;
+      z-index: 5;
+    }
+    .cta {
+      display: none;
+      @include d {
+        display: block;
+        position: absolute;
+        left: 3.5rem;
+        bottom: 6rem;
+        width: 13rem;
+        z-index: 10;
+        transition: .15s;
+        &:hover {
+          opacity: .8;
+        }
       }
+    }
+  }
+
+  @keyframes bgmobile {
+    from {
+      background-size: auto 150%;
+    }
+    to {
+      background-size: auto 110%;
+    }
+  }
+
+  @keyframes bg {
+    from {
+      background-size: 150% auto;
+    }
+    to {
+      background-size: 110% auto;
     }
   }
 
