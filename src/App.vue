12c547a0dfa3bb98d56d080d61a7977c225b98a9
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-08-30 14:58:26 +0300

    ID_51895_DK_UK_AmazonUK_AmazonPrimeVideo_LOTR_Expander

--- src/App.vue
+++ src/App.vue
@@ -1,156 +1,313 @@
 <template>
-
   <section class="section-video">
-    <ExpanderVideo :filename="videoFile" :mobile="mobile" :autoplay="true" url="" name="Main Video" />
+    <ExpanderVideo
+      :filename="videoFile"
+      :mobile="mobile"
+      :autoplay="true"
+      url="https://www.primevideo.com/detail/GTI/amzn1.dv.gti.ffefa84e-6092-433b-9f8a-79c6dd7551ae"
+      name="Main Video"
+    />
   </section>
+
+  <!-- <section class="countdown-main">
+    <div class="countdown-container">
+      <p class="countdown-title">Se serien om:</p>
+      <ExpanderCountdown />
+    </div>
+  </section> -->
 
   <section class="section-main">
-    <h1 class="heading">Some heading here</h1>
-    <p class="lead">
-      Some introductory text is placed here
-    </p>
-    <a href="" target="_blank" class="cta" data-name="Read more">Read more</a>
+    <a
+      href="https://www.primevideo.com/detail/GTI/amzn1.dv.gti.ffefa84e-6092-433b-9f8a-79c6dd7551ae"
+      target="_blank"
+      data-name="The cast"
+    >
+      <h1 class="heading">The cast</h1>
+    </a>
   </section>
 
-  <section class="section-scroller">
+  <!-- <section class="section-scroller">
     <ExpanderScroller />
-  </section>
-     
+  </section> -->
+
   <section class="section-slider">
     <ExpanderSlider />
   </section>
 
+  <!-- <section class="section-trailers">
+    <a href="" target="_blank" data-name="The cast">
+      <h1 class="heading">Trailers</h1>
+    </a>
+  </section> -->
+
+  <!-- <section class="trailer-videos">
+    <article class="items" v-for="item in items" :key="item">
+      <div class="video">
+        <ExpanderVideo
+          :filename="item.video"
+          :autoplay="false"
+          url=""
+          name="Trailer Video"
+        />
+      </div>
+    </article>
+  </section> -->
+
+  <a
+    href="https://www.primevideo.com/detail/GTI/amzn1.dv.gti.ffefa84e-6092-433b-9f8a-79c6dd7551ae"
+    target="_blank"
+    data-name="Galadriel"
+  >
+    <section class="footer-image"></section>
+  </a>
+
   <footer class="section-footer sticky">
     <div class="container">
-      <a href="" target="_blank" class="logo" data-name="Logo">
-        <img src="@/assets/logo.png">
-      </a> 
-      <a href="" target="_blank" class="cta" data-name="Read more">Read more</a>
+      <a
+        href="https://www.primevideo.com/detail/GTI/amzn1.dv.gti.ffefa84e-6092-433b-9f8a-79c6dd7551ae"
+        target="_blank"
+        class="logo"
+        data-name="Logo"
+      >
+        <img src="@/assets/logo.png" />
+      </a>
+      <!-- <a href="" target="_blank" class="cta" data-name="Read more">Read more</a> -->
     </div>
   </footer>
-
-
 </template>
 
 <script>
-  import ExpanderVideo from './components/ExpanderVideo.vue'
-  import ExpanderScroller from './components/ExpanderScroller.vue'
-  import ExpanderSlider from './components/ExpanderSlider.vue'
+import ExpanderVideo from "./components/ExpanderVideo.vue";
+// import ExpanderScroller from "./components/ExpanderScroller.vue";
+import ExpanderSlider from "./components/ExpanderSlider.vue";
+// import ExpanderCountdown from "./components/ExpanderCountdown.vue";
 
-  export default {
-    name: 'App',
-    components: {
-      ExpanderVideo,
-      ExpanderScroller,
-      ExpanderSlider,
+export default {
+  name: "App",
+  components: {
+    ExpanderVideo,
+    // ExpanderScroller,
+    ExpanderSlider,
+    // ExpanderCountdown,
+  },
+  data() {
+    return {
+      mobile: false,
+      videoFile: "1080.mp4",
+      items: [
+        {
+          video: "trailer1.mp4",
+        },
+        {
+          video: "trailer2.mp4",
+        },
+      ],
+    };
+  },
+  created() {
+    // if (window.innerWidth < 480) {
+    //   this.mobile = true
+    //   this.videofile = '480.mp4'
+    // }
+  },
+  methods: {
+    track(label, category) {
+      category = category || "Click";
+      window.customTrack(category, "interaction", label);
     },
-    data() {
-      return {
-        mobile: false,
-        videoFile: '1080.mp4'
-      }
-    },
-    created() {
-      // if (window.innerWidth < 480) {
-      //   this.mobile = true
-      //   this.videofile = '480.mp4'
-      // }
-    },
-    methods: {
-      track(label, category) {
-        category = category || 'Click'
-        window.customTrack(category, 'interaction', label)
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  @import "node_modules/minireset.css/minireset";
-  @import '@/scss/fonts.scss';
-  @import '@/scss/variables.scss';
-  @import '@/scss/mixins.scss';
+@import "node_modules/minireset.css/minireset";
+@import "@/scss/fonts.scss";
+@import "@/scss/variables.scss";
+@import "@/scss/mixins.scss";
 
-  :root {
-    --footer-height: 2rem;
-    @include d {
-      --footer-height: 4rem;
-    }
+:root {
+  --footer-height: 2rem;
+  @include d {
+    --footer-height: 4rem;
   }
+}
 
-  html {
-    font-family: $font, sans-serif;
+html {
+  font-family: $font, sans-serif;
+  font-size: 16px;
+  font-weight: $weight;
+  color: $color-text;
+  text-align: left;
+  line-height: 1.3;
+  @include desktop {
+    font-size: 1.48vw;
+  }
+  @media (min-width: $width) {
     font-size: 16px;
-    font-weight: $weight;
-    color: $color-text;
-    text-align: left;
-    line-height: 1.3;
-    @include desktop {
-      font-size: 1.48vw;
-    }
-    @media (min-width: $width) {
-      font-size: 16px;
-    }
   }
-  #app {
-    position: relative;
-    width: $width;
-    max-width: 100%;
-    margin: 0 auto;
-    background: $color-bg;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    padding-bottom: var(--footer-height);
-  }
+}
+#app {
+  position: relative;
+  width: $width;
+  max-width: 100%;
+  margin: 0 auto;
+  background: $color-bg;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding-bottom: var(--footer-height);
+}
 
-  img,
-  picture,
-  video {
-    display: block;
+img,
+picture,
+video {
+  display: block;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+p {
+  font-size: 1rem;
+}
+button {
+  font-family: $font;
+  font-size: 1rem;
+  padding: 0;
+  border: 0;
+  background: none;
+  outline: 0 none;
+  cursor: pointer;
+}
+
+.section-video {
+  padding: 3rem 4.06rem;
+  background-image: url("@/assets/background_main.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 108.375rem;
+}
+.shadow {
+  height: 12.6875rem;
+  background: linear-gradient(0deg, #000 0%, rgba(0, 0, 0, 0) 100%);
+  width: 100%;
+  margin: 0 -8.12rem;
+}
+.countdown-title {
+  color: #fff;
+  font-size: 1.8125rem;
+  text-transform: uppercase;
+  margin-left: 0.8rem;
+}
+.countdown-main {
+  margin-top: -3.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+}
+.section-main {
+  background: #000;
+  font-size: 1.8125rem;
+  color: #fff;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 2.75rem;
+}
+.section-slider {
+  background-color: #000;
+}
+.section-trailers {
+  background: #000;
+  font-size: 1.8125rem;
+  color: #fff;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 0 1rem;
+}
+.trailer-videos {
+  height: 14rem;
+  background-color: #000;
+}
+.video {
+  width: 29.8125rem;
+  height: 16.75rem;
+}
+.trailer-videos {
+  display: flex;
+  justify-content: center;
+}
+.footer-image {
+  background-image: url("@/assets/background_bottom.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 51.5625rem;
+}
+.logo {
+  width: 25rem;
+  height: auto;
+}
+
+.section-footer {
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: var(--footer-height);
+    padding: 0 1rem;
+    background: #000000;
+    height: 7.9375rem;
   }
-  a {
-    color: inherit;
-    text-decoration: none;
+  &.sticky {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 1000;
+    width: 100%;
+    .container {
+      max-width: $width;
+      margin: 0 auto;
+    }
   }
-  p {
-    font-size: 1rem;
+}
+
+@media (max-width: 480px) {
+  .trailer-videos {
+    flex-direction: column;
+    width: 100%;
   }
-  button {
-    font-family: $font;
-    font-size: 1rem;
+  .video {
+    width: 100%;
+    height: auto;
+  }
+  .section-video {
+    height: 168vw;
     padding: 0;
-    border: 0;
-    background: none;
-    outline: 0 none;
-    cursor: pointer;
   }
-
-</style>
-
-<style scoped lang="scss">
-  @import '@/scss/variables.scss';
-  @import '@/scss/mixins.scss';
+  .footer-image {
+    height: 88vw;
+    margin-bottom: 12vw;
+    background-color: #000;
+  }
+  .logo {
+    width: 47vw;
+    height: auto;
+  }
 
   .section-footer {
     .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: var(--footer-height);
-      padding: 0 1rem;
-      background: #fff;
-    }
-    &.sticky {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      z-index: 1000;
-      width: 100%;
-      .container {
-        max-width: $width;
-        margin: 0 auto;
-      }
+      height: 24vw;
     }
   }
+}
+</style>
 
+<style scoped lang="scss">
+@import "@/scss/variables.scss";
+@import "@/scss/mixins.scss";
 </style>
--- src/components/ExpanderSlider.vue
+++ src/components/ExpanderSlider.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="wrapper">
     <div class="slider" ref="slider">
-      <div class="item" v-for="item in 5" :key="item">
-        <a href="" target="_blank" class="inner" @click="$emit('track', 'test')" :data-name="`Slider - ${item}`">
-          <img :src="getImage(item)">
+      <div class="item" v-for="item in 7" :key="item">
+        <a
+          href="https://www.primevideo.com/detail/GTI/amzn1.dv.gti.ffefa84e-6092-433b-9f8a-79c6dd7551ae"
+          target="_blank"
+          class="inner"
+          @click="$emit('track', 'test')"
+          :data-name="`Slider - ${item}`"
+        >
+          <img :src="getImage(item)" />
         </a>
       </div>
     </div>
@@ -11,77 +17,167 @@
 </template>
 
 <script>
-  import {tns} from 'tiny-slider'
+import { tns } from "tiny-slider";
 
-  export default {
-    name: 'ExpanderSlider',
-    emits: ['track'],
-    mounted() {
-      tns({
-        container: this.$refs.slider,
-        items: 1,
-        gutter: 20,
-        controls: false,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayButtonOutput: false,
-        speed: 750,
-        loop: true
-      });
+export default {
+  name: "ExpanderSlider",
+  emits: ["track"],
+  mounted() {
+    tns({
+      container: this.$refs.slider,
+      items: 1,
+      gutter: 30,
+      controls: true,
+      nav: false,
+      autoplay: false,
+      autoplayTimeout: 2000,
+      autoplayButtonOutput: false,
+      speed: 900,
+      loop: true,
+      responsive: {
+        480: {
+          items: 5,
+          gutter: 18,
+        },
+      },
+    });
+  },
+  methods: {
+    getImage(image) {
+      return require(`@/assets/image-${image}.png`);
     },
-    methods: {
-      getImage(image) {
-        return require(`@/assets/${image}`)
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  @import "node_modules/tiny-slider/src/tiny-slider";
+@import "node_modules/tiny-slider/src/tiny-slider";
 </style>
 
 <style scoped lang="scss">
-  @import '@/scss/variables.scss';
-  @import '@/scss/mixins.scss';
+@import "@/scss/variables.scss";
+@import "@/scss/mixins.scss";
 
-  .wrapper {
-    position: relative;
-    .inner {
+.wrapper {
+  position: relative;
+  margin: 0 4.18rem;
+  .inner {
+    display: block;
+    // margin: 0 5rem;
+    img {
       display: block;
-      img {
-        display: block;
-        width: 100%;
+      width: 100%;
+    }
+  }
+
+  :deep(.tns-controls) {
+    outline: 0;
+    button {
+      position: absolute;
+      width: 2.30413rem;
+      height: 2.30413rem;
+      background: none;
+      border: 0;
+      text-indent: -2000%;
+      overflow: hidden;
+      border-radius: 50%;
+      // background-color: $color-2 !important;
+      transform: translate(0, -50%);
+      &:first-child {
+        left: 0.5rem;
+        background: url("../assets/arr-l.svg") no-repeat center center / contain;
+        @include d {
+          left: -3.2rem;
+          margin-top: 5.5rem;
+        }
+      }
+      &:last-child {
+        right: -3.2rem;
+        margin-top: 5.5rem;
+        background: url("../assets/arr-r.svg") no-repeat center center / contain;
+        @include m {
+          right: -1rem;
+        }
+      }
+      &[disabled] {
+        display: none;
       }
     }
-    
+  }
+
+  :deep(.tns-nav) {
+    outline: 0;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    bottom: 3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    button {
+      width: 0.5rem;
+      height: 0.5rem;
+      margin: 0 0.375rem;
+      background: #888;
+      border-radius: 50%;
+    }
+    .tns-nav-active {
+      background: #fff;
+    }
+  }
+}
+.tns-controls {
+  margin: 0 -5rem !important;
+}
+
+@media (max-width: 480px) {
+  .wrapper {
+    position: relative;
+    margin: 0; // Adjust margin for mobile screens
+    .inner {
+      display: block;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 0;
+      img {
+        display: block;
+        width: 50vw; // Set constant width for images
+        height: auto;
+        // aspect-ratio: 1 / 1;
+        z-index: 0;
+      }
+    }
+
     :deep(.tns-controls) {
       outline: 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      transform: translateY(-50%);
+      z-index: 10; // Ensure controls are above the images
       button {
-        position: absolute;
-        height: 1.75rem;
-        width: 1.75rem;
+        width: 2.30413rem;
+        height: 2.30413rem;
         background: none;
         border: 0;
         text-indent: -2000%;
         overflow: hidden;
         border-radius: 50%;
-        background-color: $color-2 !important;
-        transform: translate(0, -50%);
         &:first-child {
-          left: .5rem;
-          // background: url('../assets/arr-l.svg') no-repeat center center / contain;
-          @include d {
-            left: 1rem;
-          }
+          left: 2.5rem !important;
+          background: url("../assets/arr-l.svg") no-repeat center center /
+            contain;
+          margin-top: -5rem;
         }
         &:last-child {
-          right: .5rem;
-          // background: url('../assets/arr-r.svg') no-repeat center center / contain;
-          @include m {
-            right: 1rem;
-          }
+          right: 2.5rem !important;
+          background: url("../assets/arr-r.svg") no-repeat center center /
+            contain;
+          margin-top: -5rem !important;
         }
         &[disabled] {
           display: none;
@@ -99,9 +195,9 @@
       justify-content: center;
       align-items: center;
       button {
-        width: .5rem;
-        height: .5rem;
-        margin: 0 .375rem;
+        width: 0.5rem;
+        height: 0.5rem;
+        margin: 0 0.375rem;
         background: #888;
         border-radius: 50%;
       }
@@ -111,5 +207,8 @@
     }
   }
 
+  .wrapped .tns-controls button:last-child {
+    right: 0;
+  }
+}
 </style>
-
