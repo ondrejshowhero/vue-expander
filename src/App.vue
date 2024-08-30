e2995da7dc12ae1e824b9c637a052d2bdf63b624
Ondrej Muzik <ondrej@kizum-146.docsis.vodafone.cz>
2024-08-30 08:58:31 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
 
   <section class="section-video">
-    <ExpanderVideo :filename="videoFile" :mobile="mobile" :autoplay="true" url="" name="Main Video" />
+    <ExpanderVideo :filename="videoFile" :mobile="mobile" :autoplay="true" url="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" name="Main Video" />
   </section>
 
   <section class="section-main">
-    <h1 class="heading">Some heading here</h1>
+    <h1 class="heading">Har du tänkt på ditt barns framtid?</h1>
     <p class="lead">
-      Some introductory text is placed here
+      Att månadsspara är ett bra sätt att se till att sparandet till barnen blir av. Oavsett vad pengarna ska användas för i framtiden är det bra att börja så tidigt som möjligt.
     </p>
-    <a href="" target="_blank" class="cta" data-name="Read more">Read more</a>
+    <div class="tiles">
+      <div class="image">
+        <img src="@/assets/image.jpg">
+      </div>
+      <div class="text">
+        <h2 class="title">Spara bekvämt i vår fond - LF&nbsp;Barnspar</h2>
+        <p>
+          LF Barnspar 2045 är perfekt för dig som nyligen blivit förälder och vill starta ett enkelt sparande för ditt barn.
+        </p>
+        <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" class="cta" data-name="Börja spara i fonden">Börja spara i fonden</a>
+      </div>
+    </div>
   </section>
 
-  <section class="section-scroller">
-    <ExpanderScroller />
+  <section class="section-faq">
+    <article class="item">
+      <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" data-name="Se tabellen för sparande">
+        <h1 class="heading">Hur mycket ska jag spara?</h1>
+        <div class="content">
+          <p>Hur mycket just du kan och bör spara beror självklart på dina ekonomiska förutsättningar. Du bör även ta hänsyn till hur länge du planerar att spara, och vad målet med sparandet är. </p>
+        </div>
+        <div class="cta">Se tabellen för sparande</div>
+      </a>
+    </article>
+    <article class="item">
+      <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" data-name="Läs om kapitalförsäkring">
+        <h1 class="heading">Kan jag spara i mitt namn?</h1>
+        <div class="content">
+          <p>Du kan själv välja när barnet ska få tillgång till pengarna.</p>
+          <p>Vanligaste kontot för barnsparande i ditt namn är kapitalförsäkring, där barnet står som förmånstagare.</p>
+        </div>
+        <div class="cta">Läs om kapitalförsäkring</div>
+      </a>
+    </article>
+    <article class="item">
+      <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" data-name="Läs om investeringssparkonto">
+        <h1 class="heading">Kan jag spara i mitt barns namn?</h1>
+        <div class="content">
+          <p>Du kan spara i ditt barns namn. Juridiskt sett är pengarna barnets, och från att barnet fyller 18 år bestämmer barnet helt själv över sitt kapital.</p>
+          <p>Vanligaste kontot för sparande i barnets namn är ett investeringssparkonto(ISK).</p>
+        </div>
+        <div class="cta">Läs om investeringssparkonto</div>
+      </a>
+    </article>
   </section>
      
-  <section class="section-slider">
-    <ExpanderSlider />
+  <section class="section-links">
+    <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" class="item" data-name="Vår sparskola">
+      <p class="title">Vår sparskola</p>
+      <img src="@/assets/image-1.jpg">
+    </a>
+    <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" class="item" data-name="Ränta på ränta">
+      <p class="title">Ränta på ränta - en snöbollseffekt</p>
+      <img src="@/assets/image-2.jpg">
+    </a>
+    <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" class="item" data-name="Månadsspara">
+      <p class="title">Månadsspara</p>
+      <img src="@/assets/image-3.jpg">
+    </a>
   </section>
 
-  <footer class="section-footer sticky">
-    <div class="container">
-      <a href="" target="_blank" class="logo" data-name="Logo">
-        <img src="@/assets/logo.png">
-      </a> 
-      <a href="" target="_blank" class="cta" data-name="Read more">Read more</a>
-    </div>
+  <footer class="section-footer">
+    <a href="https://www.lansforsakringar.se/privat/bank/spara/spara-till-barn/#sparabekvmtivrfondlfbarnspar?utm_source=showheroes&utm_medium=display&utm_campaign=Nationell_TKM_Bank-Spara_PP_Barnspar_20240902_20241027&utm_content=Barnspar" target="_blank" class="logo" data-name="Logo">
+      <img src="@/assets/logo.png">
+    </a> 
+    <p class="note">
+      Historisk avkastning är ingen garanti för framtida avkastning. De pengar som placeras i fonder och aktier kan båda öka och minska i värde och det är inte säkert att du får tillbaka hela det insatta kapitalet. Du hittar fondens faktablad och informationsbroschyr på www.lansforsakringar.se
+    </p>
+    <p class="imprint">
+      Länsförsäkringar Fondförvaltning AB (Publ)<br>
+      Org.nr 556364-2783
+    </p>
   </footer>
 
 
@@ -34,15 +88,11 @@
 
 <script>
   import ExpanderVideo from './components/ExpanderVideo.vue'
-  import ExpanderScroller from './components/ExpanderScroller.vue'
-  import ExpanderSlider from './components/ExpanderSlider.vue'
 
   export default {
     name: 'App',
     components: {
       ExpanderVideo,
-      ExpanderScroller,
-      ExpanderSlider,
     },
     data() {
       return {
@@ -71,13 +121,6 @@
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
-  :root {
-    --footer-height: 2rem;
-    @include d {
-      --footer-height: 4rem;
-    }
-  }
-
   html {
     font-family: $font, sans-serif;
     font-size: 16px;
@@ -100,7 +143,6 @@
     background: $color-bg;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    padding-bottom: var(--footer-height);
   }
 
   img,
@@ -114,6 +156,7 @@
   }
   p {
     font-size: 1rem;
+    line-height: 1.3125;
   }
   button {
     font-family: $font;
@@ -131,25 +174,205 @@
   @import '@/scss/variables.scss';
   @import '@/scss/mixins.scss';
 
-  .section-footer {
-    .container {
+  .cta {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: .375rem 0;
+    gap: 0.5625rem;
+    color: #00427A;
+    font-size: 1.25rem;
+    font-weight: bold;
+    &::after {
+      content: '';
+      width: 1.5rem;
+      height: .75rem;
+      background: url('@/assets/arrow.png') no-repeat 200% center / auto 100%;
+      transition: background-position .2s;
+    }
+    &:hover {
+      &::after {
+        background-position: 50% center;
+      }
+    }
+  }
+
+  .section-main {
+    padding: 2.25rem 1.5rem 3rem;
+    @include d {
+      padding: 4.25rem 2.75rem 5.25rem;
+    }
+    .heading {
+      margin-bottom: 1rem;
+      font-family: $font-headings;
+      font-size: 2rem;
+      color: $color-2;
+      text-align: center;
+      line-height: 1.1;
+      @include d {
+        font-size: 2.5rem;
+        line-height: 1.3;
+      }
+    }
+    .lead {
+      max-width: 35rem;
+      margin-bottom: 2rem;
+      text-align: center;
+      @include d {
+        margin: 0 auto 5.25rem;
+      }
+    }
+    .tiles {
+      @include d {
+        display: flex;
+        justify-content: start;
+        align-items: center;
+      }
+      .image {
+        @include d {
+          width: 30.4375rem;
+        }
+      }
+      .text {
+        @include d {
+          width: 25.8125rem;
+          padding-left: 2.375rem;
+        }
+        .title {
+          margin-top: 1.5rem;
+          margin-bottom: .5rem;
+          font-size: 1.43625rem;
+          font-weight: bold;
+          color: $color-1;
+          @include d {
+            margin-top: 0;
+            margin-bottom: 0;
+            font-size: 1.43625rem;
+            line-height: 2;
+          }
+        }
+        p {
+          margin-bottom: 1rem;
+          @include d {
+            margin-bottom: 2rem;
+          }
+        }
+      }
+    }
+  }
+
+  .section-faq {
+    padding: 0 1.5rem;
+    @include d {
+      padding: 0 2.75rem;
       display: flex;
       justify-content: space-between;
-      align-items: center;
-      height: var(--footer-height);
-      padding: 0 1rem;
-      background: #fff;
     }
-    &.sticky {
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      z-index: 1000;
-      width: 100%;
-      .container {
-        max-width: $width;
-        margin: 0 auto;
+    .item {
+      position: relative;
+      background: $color-bg;
+      border-radius: 0.6875rem 0.6875rem 0rem 0rem;
+      box-shadow: 0rem .25rem .9563rem 0rem rgba(0, 0, 0, 0.25);
+      transition: box-shadow .15s;
+      @include d {
+        width: 20rem;
       }
+      .heading {
+        padding: 1.25rem .5rem;
+        background: $color-1;
+        font-size: 1.25rem;
+        font-weight: bold;
+        color: #fff;
+        text-align: center;
+        border-radius: 0.6875rem 0.6875rem 0rem 0rem;
+      }
+      .content {
+        padding: 1.375rem 1.375rem 4.5rem;
+        p {
+          margin-bottom: 1.5rem;
+        }
+      }
+      .cta {
+        position: absolute;
+        left: 1.25rem;
+        bottom: 1.5rem;
+      }
+      &:hover {
+        box-shadow: 0rem .5rem .9563rem 0rem rgba(0, 0, 0, 0.25);
+        .cta {
+          &::after {
+            background-position: 50% center;
+          }
+        }
+      }
+    }
+    .item + .item {
+      @include m {
+        margin-top: 2rem;
+      }
+    }
+  }
+
+  .section-links {
+    padding: 2rem 1.5rem;
+    @include d {
+      display: flex;
+      justify-content: space-between;
+      padding: 5.375rem 2.75rem 3.75rem;
+    }
+    .item {
+      display: block;
+      position: relative;
+      overflow: hidden;
+      @include d {
+        width: 20rem;
+      }
+      img {
+        transition: transform .4s;
+      }
+      p {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 50%;
+        padding: 5rem 1.25rem 0;
+        z-index: 2;
+        color: #fff;
+        font-family: $font-headings;
+        font-size: 1.5rem;
+        background: linear-gradient(180deg, rgba(0, 66, 122, 0.00) 0%, #005CA9 93%);
+      }
+      &:hover {
+        img {
+          transform: scale(1.05);
+        }
+      }
+    }
+    .item + .item {
+      @include m {
+        margin-top: 2rem;
+      }
+    }
+  }
+
+  .section-footer {
+    padding: 1rem 1.5rem 3rem;
+    text-align: center;
+    @include d {
+      padding: 0 0 3rem;
+    }
+    .logo {
+      display: block;
+      width: 4.5rem;
+      margin: 0 auto;
+    }
+    .note {
+      max-width: 41.25rem;
+      margin: 1.8125rem auto 1.5rem;
+    }
+    .imprint {
+      font-size: .875rem;
     }
   }
 
