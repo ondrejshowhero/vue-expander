eac9a068cf1691b20b717826c45b652628000218
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2025-01-03 20:09:28 +0200

    Add changes for Nicotinell campaign week 1 of 2024

---
+++ src/App.vue
@@ -0,0 +1,376 @@
+<template>
+  <section class="section-video">
+    <ExpanderVideo
+      filename="1080.mp4"
+      :autoplay="true"
+      url="https://ad.doubleclick.net/ddm/trackclk/N916230.4615535SHOWHEROESDK/B32917075.411996170;dc_trk_aid=604171701;dc_trk_cid=228135749;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1"
+    />
+  </section>
+
+  <section class="section-main">
+    <div class="text">
+      <div class="logo">
+        <img src="@/assets/nicotinell.png" />
+      </div>
+      <h1 class="heading">Nicotinell Spray</h1>
+      <h2 class="subheading">
+        Den hurtige metode til lindring af ryge- og nikotintrang
+      </h2>
+      <p class="lead">
+        Nicotinell Spray begynder at lindre ryge- og nikotintrang efter kun 30
+        sekunder. Det er en hurtig, nem og diskret metode til at bevare
+        kontrollen over din rygetrang og fokusere på, hvad du vil og skal gøre.
+        Takket være det praktiske lommeformat kan du kan nemt have sprayen med
+        dig overalt, så du kan lindre din rygetrang, hvor end den melder sig.
+        Nicotinell spray fås i enkelt- eller dobbeltpakninger, begge med en
+        frisk myntesmag, og er din hjælp til at blive røgfri.
+      </p>
+      <div class="footer">
+        <a
+          href="https://ad.doubleclick.net/ddm/trackclk/N916230.4615535SHOWHEROESDK/B32917075.411996170;dc_trk_aid=604171701;dc_trk_cid=228135749;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1"
+          target="_blank"
+          class="cta"
+          data-name="Køb her"
+          >Køb her</a
+        >
+        <p class="info">
+          1mg<br />
+          Enkelt- eller dobbeltpakning
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <a
+        href="https://ad.doubleclick.net/ddm/trackclk/N916230.4615535SHOWHEROESDK/B32917075.411996170;dc_trk_aid=604171701;dc_trk_cid=228135749;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1"
+        target="_blank"
+        class="link"
+        data-name="Image"
+      >
+        <img src="@/assets/product.png" />
+      </a>
+    </div>
+  </section>
+
+  <div class="bg">
+    <section class="section-image">
+      <a
+        href="https://ad.doubleclick.net/ddm/trackclk/N916230.4615535SHOWHEROESDK/B32917075.411996170;dc_trk_aid=604171701;dc_trk_cid=228135749;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1"
+        target="_blank"
+        class="link"
+        data-name="Image"
+      >
+        <img src="@/assets/image.jpg" />
+      </a>
+    </section>
+
+    <footer class="section-footer">
+      <a
+        href="https://ad.doubleclick.net/ddm/trackclk/N916230.4615535SHOWHEROESDK/B32917075.411996170;dc_trk_aid=604171701;dc_trk_cid=228135749;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1"
+        target="_blank"
+        class="logo"
+        data-name="Logo"
+      >
+        <img src="@/assets/haleon.svg" />
+      </a>
+      <p class="legal">
+        Nicotinell (nikotin) plaster (pl), tyggegummi (tg) og sugetabletter
+        (st). Anvendelse: pl: Hjælpemiddel i forbindelse med tobaksafvænning. tg
+        og st: Til behandling af tobaksafhængighed ved at lette nikotintrang og
+        -abstinenser ved tobaksafvænning eller ved rygereduktion. Dosering: pl:
+        Dosis bør vælges ud fra brugerens nikotinafhængighed. Behandlingstiden
+        er individuel. Frigiver nikotin gennem hele døgnet og påsættes ren, tør,
+        hårfri og ubeskadiget hud. Plastret må ikke påsættes et rødt, beskadiget
+        eller irriteret hudområde. For at undgå lokal irritation af huden bør
+        brugen af samme hudområde undgås for mindst 7 dage. 24 timers anvendelse
+        anbefales for at undgå rygetrang om morgenen, men hvis ønsket kan det
+        tages af ved sengetid. Bør ikke anvendes i mere end 12 måneder uden
+        lægens anbefaling. Det anbefales at søge rådgivning fra læge eller
+        apotekspersonalet. tg: Dosis bør vælges ud fra brugerens
+        nikotinafhængighed, dog højst 15 stk. a 4 mg pr. dag eller 24 stk. a 2
+        mg pr. dag. Behandlingstiden er individuel. Brug ud over 1 år anbefales
+        ikke. Det anbefales at søge rådgivning fra læge eller apotekspersonalet.
+        Frigiver nikotin ved tygning, hvorfor korrekt tyggeteknik er vigtig. st:
+        Dosis bør vælges ud fra brugerens nikotinafhængighed, dog er den
+        maximale daglige dosis (1mg) 24 stk. (2mg) 15 stk. Behandlingstiden er
+        individuel. Det anbefales at søge rådgivning fra læge eller
+        apotekspersonalet. Frigiver nikotin ved sugning, hvorfor korrekt
+        sugeteknik er vigtig. Forsigtighedsregler: Må ikke anvendes ved
+        overfølsomhed over for indholdsstoffer eller af ikke-rygere. Bør ikke
+        anvendes af børn under 18 år samt under graviditet og amning uden lægens
+        anvisning. Forsigtighed udvises ved nylig hjerte-, lever eller
+        nyreproblemer, samt ved slagtilfælde, højt blodtryk, overaktive binyrer,
+        sukkersyge, højt stofskifte, sår på tolvfingertarmen, et betændt
+        spiserør, mavesår eller ved tidligere krampeanfald. pl: Et rygestop bør
+        forsøges uden brug af plasteret, medmindre lægen anbefaler det.
+        Patienter med sukkersyge bør overvåge blodsukkerniveau oftere end
+        normalt ved ibrugtagen af Nicotinell depotplaster. Patienter som har
+        problemer med hjertet, skal kun bruge Nicotinell kombinationsbehandling
+        hvis lægen anbefaler det. Forsigtighed udvises hos patienter der har
+        oplevet krampeanfald, samt hos patienter med kraftig rødme, kløe, udslæt
+        eller hævelse dér, hvor plastret har siddet. Ophør brugen af plastret og
+        konsulter lægen hvis rødmen i huden, hævelse eller udslæt opstår. tg:
+        Indeholder sorbitol (E420) og bør derfor ikke anvendes til patienter med
+        hereditær fruktoseintoleranse. Tg: indeholder butylhydroxytoluen (E321),
+        der kan forårsage lokalirritation af slimhinderne. pl: Forsigtighed
+        udvises hos patienter med kraftig rødme, kløe, udslæt eller hævelse dér,
+        hvor plastret har siddet. st: Indeholder aspartam og må ikke anvendes
+        til patienter med phenylketonouri (Følings sygdom). Indeholder maltitol
+        (E965) og bør ikke anvendes til patienter med arvelig
+        fruktoseintolerans. Maltitol kan give en mild, afførende virkning.
+        Forsigtighed bør udvises ved spiserørsbetændelse, betændelse i mund
+        eller svælg, mavesmerter, kvalme, opkastninger. Bivirkninger: pl: Meget
+        almindelige: Reaktioner på applikationsstedet, kvalme, opkastning,
+        hovedpine, svimmelhed, søvnløshed inklusiv mareridt. Almindelige:
+        nervøsitet, hjertebanken, åndenød, maveproblemer, mavesmerter,
+        forstoppelse, diarré, mundtørhed, svælgkatar, forøget svedproduktion,
+        smerte i leddene, muskelkramper, smerte på applikationsstedet, træthed.
+        Øvrige bivirkninger: Se indlægsseddel. Visse symptomer kan skyldes
+        abstinenssymptomer og for lav tilførsel af nikotin. tg: Meget
+        almindelige: Kvalme. Almindelige: betændelse i mundslimhinder, ubehag i
+        munden, ondt i halsen, opkast, mavesmerter, diarré, fordøjelsesbesvær,
+        halsbrand, luft i maven, hikke, forstoppelse, svimmelhed, hovedpine,
+        søvnløshed, hoste, mundtørhed, smerte i mund, hals og kæbemuskler.
+        Øvrige bivirkninger: Se indlægsseddel. Visse symptomer kan skyldes
+        abstinenssymptomer og for lav tilførsel af nikotin. st: Meget
+        almindelige: Kvalme. Almindelige: betændendelse i mundslimhinder, ubehag
+        i munden, ondt i halsen, opkast, ubehag i maveregionen, mavesmerter,
+        diarré, fordøjelsesbesvær, halsbrand, luft i maven, hikke, forstoppelse,
+        svimmelhed, hovedpine og søvnløshed. Øvrige bivirkninger: Se
+        indlægsseddel. Visse symptomer kan skyldes abstinenssymptomer og for lav
+        tilførsel af nikotin. Graviditet og amning: Kun efter samråd med læge.
+        Overdosering: Symptomer, der ligner dem, der forekommer ved for meget
+        rygning kan forekomme. Pakninger: pl: 7 mg/24 timer, 14 mg/24 timer og
+        21 mg/24 timer: 7 og 21 stk. tg: Spearmint: 2 og 4 mg: 24 og 204 stk.
+        IceMint: 2 og 4 mg: 24 og 204 stk. Lakrids: 2 og 4 mg: 24 og 204 stk.
+        Mint: 2 og 4 mg: 24 og 204 stk. Fruit: 2 mg: 24 og 204 stk; 4 mg: 204
+        stk. st: 1 og 2 mg: 36 og 204 stk. Læs omhyggeligt vejledningen i
+        indlægssedlen eller på pakningen. Haleon Denmark ApS, tlf: 80 25 16 27.
+        Trademarks owned or licensed by Haleon. ©2023 Haleon or licensor.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import ExpanderVideo from "./components/ExpanderVideo.vue";
+
+export default {
+  name: "App",
+  components: {
+    ExpanderVideo,
+  },
+  data() {
+    return {
+      mobile: false,
+    };
+  },
+  created() {
+    if (window.innerWidth < 480) {
+      this.mobile = true;
+    }
+  },
+  methods: {
+    track(label) {
+      return label;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "node_modules/minireset.css/minireset";
+@import "@/scss/fonts.scss";
+@import "@/scss/variables.scss";
+@import "@/scss/mixins.scss";
+
+html {
+  font-family: $font, sans-serif;
+  font-size: 16px;
+  font-weight: $weight;
+  color: $color-text;
+  text-align: left;
+  line-height: 1.3;
+  @include desktop {
+    font-size: 1.48vw;
+  }
+  @media (min-width: $width) {
+    font-size: 16px;
+  }
+}
+#app {
+  position: relative;
+  width: $width;
+  max-width: 100%;
+  margin: 0 auto;
+  background: $color-bg;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+img {
+  display: block;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+p {
+  font-size: 1rem;
+}
+button {
+  font-family: $font;
+  font-size: 1rem;
+  padding: 0;
+  border: 0;
+  background: none;
+  outline: 0 none;
+  cursor: pointer;
+}
+</style>
+
+<style scoped lang="scss">
+@import "@/scss/variables.scss";
+@import "@/scss/mixins.scss";
+
+.section-video {
+  padding: 2rem 1rem 0;
+  background: url("@/assets/bg/bg-1.png") no-repeat center bottom / 150% auto;
+  @include d {
+    padding: 3.625rem 4rem 0;
+    background-size: 100% auto;
+  }
+}
+
+.section-main {
+  position: relative;
+  padding: 5rem 1.5rem 1.375rem;
+  background: url("@/assets/bg/shape-1.png") no-repeat center top / 150% auto,
+    url("@/assets/bg/shape-2.png") no-repeat center bottom / 150% auto;
+  @include d {
+    display: flex;
+    justify-content: space-between;
+    padding: 13.125rem 9.125rem 1.375rem;
+    background-size: 100% auto;
+    .text {
+      width: 31.5rem;
+    }
+    .image {
+      width: 16.125rem;
+    }
+  }
+  .logo {
+    position: absolute;
+    left: 50%;
+    width: 8rem;
+    top: 1rem;
+    transform: translate(-50%, 0);
+    @include d {
+      width: 13.5rem;
+      top: 2rem;
+    }
+  }
+  .text {
+    padding-top: 1.25rem;
+    .heading {
+      font-family: $font-headings;
+      font-size: 2.5rem;
+      font-weight: bold;
+      color: $color-1;
+      @include d {
+        font-size: 3.5rem;
+      }
+    }
+    .subheading {
+      margin: 0 0 0.625rem;
+      font-family: $font-headings;
+      color: #000;
+      font-size: 1.25rem;
+      font-weight: bold;
+      color: $color-2;
+      @include d {
+        font-size: 2rem;
+      }
+    }
+    .lead {
+      line-height: 1.31;
+    }
+    .footer {
+      margin-top: 1.875rem;
+      display: flex;
+      align-items: center;
+      .info {
+        margin-left: 1.5rem;
+        color: #000;
+        @include m {
+          br {
+            display: none;
+          }
+        }
+        @include d {
+          margin-left: 2.625rem;
+        }
+      }
+    }
+    .cta {
+      padding: 1.125rem 3rem 0.875rem;
+      font-family: $font-headings;
+      font-size: 1.125rem;
+      font-weight: bold;
+      color: #fff;
+      border-radius: 0.625rem;
+      background: #151f6d;
+      transition: 0.15s;
+      white-space: nowrap;
+      @include d {
+        padding: 1.125rem 3rem 0.875rem;
+      }
+      &:hover {
+        background: #fdcc10;
+        color: $color-1;
+      }
+    }
+  }
+  .image {
+    @include m {
+      margin: 3rem auto 2rem;
+      width: 10rem;
+    }
+    .link {
+      display: block;
+    }
+  }
+}
+
+.bg {
+  background: url("@/assets/bg/bg-2.png") no-repeat center top / 100% auto;
+}
+
+.section-image {
+  padding: 1rem 2rem 0;
+  @include d {
+    padding: 0 4rem;
+  }
+  .link {
+    display: block;
+  }
+}
+
+.section-footer {
+  padding: 3.25rem 4.75rem 5rem;
+  text-align: center;
+  @include m {
+    padding: 3.25rem 2rem 5rem;
+    background: linear-gradient(to bottom, rgba(#1e2d9d, 0) 0%, #1e2d9d 10%);
+  }
+  .logo {
+    margin: 0 0 3.25rem;
+    display: inline-block;
+    width: 17.75rem;
+  }
+  .legal {
+    font-size: 0.75rem;
+    color: #fff;
+    line-height: 1.75;
+  }
+}
+</style>
